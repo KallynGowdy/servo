a5a8e454a7fec6b0c9a78128363b2552510f6923
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-07-13 11:08:32 -0400

    Change the Default URL to point to auxplayer.com

--- support/hololens/ServoApp/DefaultUrl.h
+++ support/hololens/ServoApp/DefaultUrl.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define DEFAULT_URL_PROD L"https://auxplayer.com"
+
 // For development purpose.
 // Will override shell.homepage preference:
 // #define OVERRIDE_DEFAULT_URL L"data:text/html,<input>"
